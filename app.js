ba641f7de50fa2aa59c70325005510a072acd4c8
Casey O'Kane <okanecasey@gmail.com>
2016-09-20 16:03:16 -0700

    Capitals are now lower case

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 "use strict";
-var sibling = prompt('Do I have any siblings?');
+var sibling = prompt('Do I have any siblings?').toLowerCase();
 if (sibling === 'yes') {
   alert('You\'re right! I have a brother');
   console.log('You guessed correctly!');
@@ -9,7 +9,7 @@ else {
   console.log('You guessed wrong!');
 }
 
-var horses = prompt('Do I like horses?');
+var horses = prompt('Do I like horses?').toLowerCase();
 if (horses === 'yes') {
   alert('You guessed wrong!');
   console.log('You guessed wrong');
@@ -19,7 +19,7 @@ else {
   console.log('You guessed correctly!');
 }
 
-var computers = prompt('Do I like computers?');
+var computers = prompt('Do I like computers?').toLowerCase();
 if (computers === 'yes') {
   alert('You guessed right!');
   console.log('You guessed right');
@@ -29,7 +29,7 @@ else {
   console.log('You guessed wrong');
 }
 
-var hampster = prompt('Do I have a hampster?');
+var hampster = prompt('Do I have a hampster?').toLowerCase();
 if (hampster === 'yes') {
   alert('you guessed wrong!');
   console.log('You guessed wrong');
@@ -39,7 +39,7 @@ else {
   console.log('You guessed correctly');
 }
 
-var favColor = prompt('Is my favorite color blue?');
+var favColor = prompt('Is my favorite color blue?').toLowerCase();
 if (favColor === 'yes') {
   alert('You guessed right!');
   console.log('You guessed right');
