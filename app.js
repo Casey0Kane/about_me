a4a0d9494d6d2706eb8de1caab33bb3c66aa0887
Casey O'Kane <okanecasey@gmail.com>
2016-09-20 12:15:29 -0700

    scaffold html, js, css files; delete old html file

---
+++ app.js
@@ -0,0 +1,15 @@
+var username = prompt('What is your name?');
+alert('Welcome, ' + username + '!');
+console.log('The user\'s name is ' + username);
+
+var favcolor = prompt('What is your favorite color?');
+alert('Your favorite color is ' + favcolor);
+console.log('The user\'s favorite color is ' + favcolor);
+
+var quest = prompt('What is your quest?');
+alert('Your quest is ' + quest);
+console.log('The user\'s quest is ' + quest);
+
+var favfood = prompt('What is your favorite food?');
+alert('Your favorite food is ' + favfood);
+console.log('The user\'s favorite food is ' + favfood);
